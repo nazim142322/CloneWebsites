46eba695300f3af9e1219d05724623efa82c86e9
nazim142322 <nazim142322@gmail.com>
2023-05-26 23:10:51 +0530

    dark mode btn added

--- Facebook clone/script.js
+++ Facebook clone/script.js
@@ -1,4 +1,8 @@
 var settingsmenu = document.querySelector(".settings-menu")
-function settingsMenuToggle(){
+var darkBtn = document.getElementById("dark-btn")
+function settingsMenuToggle(){   
    settingsmenu.classList.toggle("settings-menu-height");
+}
+darkBtn.onclick=function(){
+   darkBtn.classList.toggle("dark-btn-on");
 }
